f3599788ba43f0b46953d1a09f44960e96df71ee
Vinícius Martins Pinto da Silva <vinicius.martins@pca.com.br>
2020-05-29 10:46:10 -0300

    Ajustando grafico doughtnut

---
+++ src/components/HeaderDoughnut/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './styles.css';
+
+function HeaderDoughnut() {
+  return (
+    <div className="header">
+      <div className="header_info">
+          <h1>949</h1>
+          <p>Número de Animais - 2020</p>
+      </div>
+      <div className="header_info">
+          <h1>387</h1>
+          <p>Número de Animais - MTD</p>
+      </div>
+  </div>
+  );
+}
+
+export default HeaderDoughnut;
--- src/components/MainChart/index.js
+++ src/components/MainChart/index.js
@@ -1,83 +1,70 @@
 import React, { Component } from 'react';
 import { Doughnut } from 'react-chartjs-2';
+import 'chart.piecelabel.js';
 
 import './styles.css';
+import HeaderDoughnut from '../HeaderDoughnut';
 
 class Chart extends Component {
 
-    constructor(props) {
-        super(props);
+  state = {
+      chartData: {
+          labels: ['Mestiço Zebu 665', 'Nelore 224', 'Industrial 60'],
+          datasets: [
+              {
+                  data: [665, 224, 60],
+                  backgroundColor: [
+                      'rgba(2, 95, 126, 1)',
+                      'rgba(1, 120, 131, 1)',
+                      'rgba(27, 151, 143, 1)'
+                  ],
+              }
+          ],
+      }
 
-        this.state = {
-            chartData: {
-                labels: ['Mestiço Zebu 665', 'Nelore 224', 'Industrial 60'],
-                datasets: [
-                    {
-                        data: [665, 224, 60],
-                        backgroundColor: [
-                            'rgba(2, 95, 126, 1)',
-                            'rgba(1, 120, 131, 1)',
-                            'rgba(27, 151, 143, 1)'
-                        ]
-                    }
-                ],
-            }
+  };
 
-        };
+  render() {
 
-    }
-
-    render() {
-
-        return (
-            <>
-                <div className="header">
-                    <div className="header_info">
-                        <h1>949</h1>
-                        <p>Número de Animais - 2020</p>
-                    </div>
-                    <div className="header_info">
-                        <h1>387</h1>
-                        <p>Número de Animais - MTD</p>
-                    </div>
-                </div>
-                <div className="container_mainChart">
-                    <div className="doughnut_chart">
-                        <Doughnut data={this.state.chartData}
-                            width={300}
-                            height={300}
-                            options={
-                                {
-                                    title: {
-                                        display: true,
-                                        text: 'Nª de Animais por Raça',
-                                        fontSize: 25,
-                                    },
-                                    legend: {
-                                        display: true,
-                                        position: 'bottom',
-                                        fullWidth: true,
-                                        align: 'start'
-
-                                    },
-                                    tooltips: {
-                                        enabled: true,
-                                        mode: 'nearest'
-                                    },
-                                    responsive: true,
-                                    maintainAspectRatio: true,
-                                    // onResize: (<Doughnut />, 300) => {
-
-                                    // }
+    return (
+        <>
+            <HeaderDoughnut />
+            <div className="container_mainChart">
+                <div className="doughnut_chart">
+                    <Doughnut data={this.state.chartData}
+                        width={350}
+                        height={350}
+                        options={
+                            {
+                                title: {
+                                  display: true,
+                                  text: 'Nª de Animais por Raça',
+                                  fontSize: 25,
+                                },
+                                legend: {
+                                  position: 'top',
+                                  fullWidth: true,
+                                },
+                                tooltips: {
+                                    enabled: true,
+                                    mode: 'nearest'
+                                },
+                                responsive: true,
+                                maintainAspectRatio: true,
+                                pieceLabel: {
+                                  render: 'value',
+                                  fontColor: 'white',
+                                  fontStyle: 'bold',
                                 }
-                            }>
+                            }
+                        }>
 
-                        </Doughnut>
-                    </div>
+                    </Doughnut>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
+  }
 }
 
 export default Chart;
