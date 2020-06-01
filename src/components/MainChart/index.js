882b3b49cff96d73a92d47133a3b76d07fe674d1
jxrxsx <jrslord@hotmail.com>
2020-06-01 10:02:19 -0300

    ajustes de responsividade dos gráficos

--- src/components/MainChart/index.js
+++ src/components/MainChart/index.js
@@ -8,76 +8,77 @@ import HeaderDoughnut from '../HeaderDoughnut';
 class Chart extends Component {
 
   state = {
-      chartData: {
-          labels: ['Mestiço Zebu', 'Nelore', 'Industrial'],
-          datasets: [
-              {
-                  data: [665, 224, 60],
-                  backgroundColor: [
-                      'rgba(2, 95, 126, 1)',
-                      'rgba(1, 120, 131, 1)',
-                      'rgba(27, 151, 143, 1)'
-                  ],
-              }
+    chartData: {
+      labels: ['Mestiço Zebu', 'Nelore', 'Industrial'],
+      datasets: [
+        {
+          data: [665, 224, 60],
+          backgroundColor: [
+            'rgba(2, 95, 126, 1)',
+            'rgba(1, 120, 131, 1)',
+            'rgba(27, 151, 143, 1)'
           ],
-      }
+        }
+      ],
+    }
 
   };
 
   render() {
 
     return (
-        <>
-            <HeaderDoughnut />
-            <div className="container_mainChart">
-                <div className="doughnut_chart">
-                    <Doughnut data={this.state.chartData}
-                        width={410}
-                        height={350}
-                        options={
-                            {
-                                title: {
-                                  display: true,
-                                  text: 'Nª de Animais por Raça',
-                                  fontSize: 25,
-                                },
-                                legend: {
-                                  position: 'bottom'
-                                },
-                                tooltips: {
-                                    enabled: false,
-                                },
-                                responsive: true,
-                                pieceLabel: {
-                                  render: (args) => {return( args.label+" ("+args.value+")")},
-                                  fontColor: 'black',
-                                  fontStyle: 'bold',
-                                  position: 'outside',
-                                //   overlap: true
-                                },
-                                plugins: {
-                                  datalabels: {
-                                    color: '#eee',
-                                    fontWeight: 'bold',
-                                    value: 'percentage',
-                                    formatter: (value, ctx) => {
-                                      let sum = 0;
-                                      let dataArr = ctx.chart.data.datasets[0].data;
-                                      dataArr.map(data => {
-                                          sum += data;
-                                      });
-                                      let percentage = (value*100 / sum).toFixed(2)+"%";
-                                      return percentage;
-                                    },
-                                  }
-                                }
-                            }
-                        }>
+      <>
+        <HeaderDoughnut />
+        <div className="container_mainChart">
+          <div className="doughnut_chart">
+            <Doughnut data={this.state.chartData}
+              width={410}
+              height={350}
+              options={
+                {
+                  maintainAspectRatio: true,
+                  responsive: true,
+                  title: {
+                    display: true,
+                    text: 'Nª de Animais por Raça',
+                    fontSize: 25,
+                  },
+                  legend: {
+                    position: 'bottom'
+                  },
+                  tooltips: {
+                    enabled: false,
+                  },
+                  pieceLabel: {
+                    render: (args) => { return (args.label + " (" + args.value + ")") },
+                    fontColor: 'black',
+                    fontStyle: 'bold',
+                    position: 'outside',
+                    //   overlap: true
+                  },
+                  plugins: {
+                    datalabels: {
+                      color: '#eee',
+                      fontWeight: 'bold',
+                      value: 'percentage',
+                      formatter: (value, ctx) => {
+                        let sum = 0;
+                        let dataArr = ctx.chart.data.datasets[0].data;
+                        dataArr.map(data => {
+                          sum += data;
+                        });
+                        let percentage = (value * 100 / sum).toFixed(2) + "%";
+                        return percentage;
+                      },
+                    }
+                  }
+                }
+              }>
 
-                    </Doughnut>
-                </div>
-            </div>
-        </>
+            </Doughnut>
+          </div>
+        </div>
+      </>
     );
   }
 }
